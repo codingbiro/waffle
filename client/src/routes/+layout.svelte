c8a7713b90c0d9bcdadcdf83206b8c59ccca7589
codingbiro <hello@birovince.com>
2023-07-11 01:26:43 +0200

    feat: add svelte client app

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,59 @@
+<nav>
+	<p class="logo">Waffle</p>
+	<a href="/dashboard"><span class="material-symbols-outlined">&#xe871</span> Dashboard</a>
+	<p class="bottom">v0.0.1</p>
+</nav>
+
+<article>
+	<slot />
+</article>
+
+<style>
+	:root {
+		font-family: 'Roboto', sans-serif;
+	}
+	nav {
+		position: fixed;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		width: 150px;
+		z-index: 2;
+		background-color: rgb(150, 120, 0);
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		padding-top: 20px;
+	}
+	article {
+		margin-left: 140px;
+		padding: 10px;
+	}
+	a,
+	a:visited,
+	a:active,
+	a:hover {
+		text-decoration: none;
+		color: white;
+		font-size: 16px;
+		font-weight: 300;
+		display: flex;
+		align-items: center;
+		& > span {
+			margin-right: 5px;
+		}
+	}
+	a:hover {
+		opacity: 0.6;
+	}
+	p.logo {
+		font-weight: 900;
+		font-size: 20px;
+		border-bottom: solid 1px black;
+		margin-bottom: 40px;
+	}
+	p.bottom {
+		margin-top: auto;
+		font-weight: 300;
+	}
+</style>
---
+++ client/src/routes/dashboard/+page.svelte
@@ -0,0 +1,61 @@
+<h1>Dashboard</h1>
+<div class="top-section">
+	<button>Create Update <span class="material-symbols-outlined">&#xe2c3</span></button>
+</div>
+<table>
+	<tr>
+		<th>Version</th>
+		<th>Uploader</th>
+		<th>Date</th>
+		<th>Status</th>
+	</tr>
+	<tr>
+		<td colspan="4">No data</td>
+	</tr>
+</table>
+
+<style>
+	div.top-section {
+		display: flex;
+		justify-content: flex-end;
+		padding: 20px 50px;
+	}
+	h1 {
+		text-align: center;
+	}
+	button {
+		display: flex;
+		align-items: center;
+		& > span {
+			margin-left: 10px;
+		}
+		padding: 5px 20px;
+		text-transform: uppercase;
+		font-family: Roboto, sans-serif;
+		background-color: slategray;
+		color: white;
+		border: none;
+		cursor: pointer;
+		border-radius: 5px;
+	}
+	button:hover {
+		opacity: 0.6;
+	}
+	table {
+		font-family: Roboto, sans-serif;
+		border-collapse: collapse;
+		width: 100%;
+	}
+	td,
+	th {
+		border: 1px solid #dddddd;
+		text-align: left;
+		padding: 8px;
+	}
+	td {
+		text-align: center;
+	}
+	tr:nth-child(even) {
+		background-color: #dddddd;
+	}
+</style>
