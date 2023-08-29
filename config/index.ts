2472fa0e368c0fba8b535866ac95dfce1ff33e87
Vince <hello@birovince.com>
2023-08-29 23:35:34 +0200

    fix: filter port number newline char

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import abi from './FirmwareUdpdatesAbi';
 
 const deployedAddress = fs.readFileSync('./config/DeployedContractAddress.bin', 'utf8');
-const PORT = fs.readFileSync('./config/PORT', 'utf8') || 8545;
+const PORT = (fs.readFileSync('./config/PORT', 'utf8').substring(0,4)) || 8545;
 
 const contractName = 'FirmwareUpdates';
 const networkAddress = 'http://127.0.0.1:' + PORT;
